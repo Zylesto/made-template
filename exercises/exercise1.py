1125b0a158df06244d8bf78b80bb8ebac130821c
Felix Berger <felix.berger98@icloud.com>
2023-11-11 17:17:17 +0100

    Rewrote it

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,14 +1,32 @@
 import pandas as pd
-from sqlalchemy import INTEGER, String,Float, DECIMAL
+from sqlalchemy import create_engine, Table, Column, MetaData, Integer, String, Float
 
-data_url = "https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv"
-df = pd.read_csv(data_url, sep=';')
+df = pd.read_csv(
+    'https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv',
+    engine='python',
+    on_bad_lines='skip',
+    sep=';'
+)
 
-df.head()
+engine = create_engine('sqlite:///airports.sqlite')
+metadata = MetaData()
 
-columnTypes = {'column_1': INTEGER, 'column_2': String, 'column_3': String, 'column_4': String, 'column_5': String,
-               'column_6': String, 'column_7': Float,'column_8': Float, 'column_9': INTEGER, 'column_10': Float,
-               'column_11': String,'column_12': String, 'geo_punkt': Float}
+airports = Table('airports', metadata,
+                 Column('column_1', Integer),
+                 Column('column_2', String),
+                 Column('column_3', String),
+                 Column('column_4', String),
+                 Column('column_5', String),
+                 Column('column_6', String),
+                 Column('column_7', Float),
+                 Column('column_8', Float),
+                 Column('column_9', Integer),
+                 Column('column_10', Float),
+                 Column('column_11', String),
+                 Column('column_12', String),
+                 Column('geo_punkt', Float),
+                 )
 
+metadata.create_all(engine)
 
-df.to_sql('airports','airports.sqlite', if_exists='replace', index=False)
+df.to_sql('airports', con=engine, if_exists='replace', index=False)
