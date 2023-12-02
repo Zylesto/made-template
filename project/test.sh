c46309b46c5d357ed11037511e75a90e900665c0
Felix Berger <felix.berger98@icloud.com>
2023-12-02 13:51:25 +0100

    Added tests, fixed pipeline

---
+++ project/test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+data_file="../data/zylesto.sqlite"
+
+is_valid_sqlite_file() {
+
+    if [ ! -f "$data_file" ]; then
+        echo "Error: The specified path '$data_file' does not exist or is not a regular file path."
+        return 1
+    elif [ ! -r "$data_file" ]; then
+        echo "Error: The file '$data_file' is not readable."
+        return 1
+    fi
+
+    local file_header=$(head -c 16 "$data_file")
+    if [[ "$file_header" == "SQLite format 3"* ]]; then
+        echo "The file $data_file is a valid SQLite database."
+        return 0
+    else
+        echo "The file $data_file is not a valid SQLite database."
+        return 1
+    fi
+}
+
+is_valid_sqlite_file
+
+
