78054c6ef33cdfb4f5ac43f7639fe7a5c22c6c4d
Felix Berger <felix.berger98@icloud.com>
2023-12-02 15:07:51 +0100

    Fixed errors

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -4,18 +4,12 @@ import requests
 from io import StringIO
 
 
-def download_csv(url):
-    response = requests.get(url)
-    return response.content.decode('ISO-8859-1')
-
-
 def process_data(csv_content):
     df = pd.read_csv(StringIO(csv_content), sep=';', encoding='iso-8859-1', skiprows=6, skipfooter=4, engine='python')
 
     df = df.iloc[:, [0, 1, 2, 12, 22, 32, 42, 52, 62, 72]]
     df.columns = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
 
-    # Daten validieren
     df['CIN'] = df['CIN'].astype(str).str.zfill(5)
     numeric_columns = ['petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
     for col in numeric_columns:
@@ -25,8 +19,8 @@ def process_data(csv_content):
     return df
 
 
-def save_to_database(df, db_name, table_name):
-    engine = sqlalchemy.create_engine(f'sqlite:///{db_name}')
+def save_to_database(df):
+    engine = sqlalchemy.create_engine('sqlite:///cars.sqlite')
     column_types = {
         'date': sqlalchemy.String(20),
         'CIN': sqlalchemy.String(255),
@@ -39,10 +33,12 @@ def save_to_database(df, db_name, table_name):
         'plugInHybrid': sqlalchemy.Integer,
         'others': sqlalchemy.Integer
     }
-    df.to_sql(name=table_name, con=engine, if_exists='replace', index=False, dtype=column_types)
+
+    df.to_sql(con=engine, name='cars', if_exists='replace', index=False, dtype=column_types)
 
 
-    url = 'https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv'
-    csv_content = download_csv(url)
-    df = process_data(csv_content)
-    save_to_database(df, 'cars.sqlite', 'cars')
+csv_content = requests.get('https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv').text
+
+df = process_data(csv_content)
+
+save_to_database(df)
