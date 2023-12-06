e3fa141500b5eed0bc964c8404b330dc7a851944
Felix Berger <felix.berger98@icloud.com>
2023-12-06 10:10:00 +0100

    Reworked tests

--- project/test.py
+++ project/test.py
@@ -1,5 +1,6 @@
 import sqlite3
 import pytest
+import os
 
 @pytest.fixture
 def db_cursor():
@@ -7,6 +8,23 @@ def db_cursor():
     cursor = db_connection.cursor()
     yield cursor
     db_connection.close()
+
+@pytest.fixture
+def db_connection():
+    db_path = "../data/zylesto.sqlite"
+    if not os.path.isfile(db_path):
+        pytest.fail(f"Error: The specified path '{db_path}' does not exist or is not a regular file path.")
+    try:
+        connection = sqlite3.connect(db_path)
+        yield connection
+    finally:
+        connection.close()
+
+def test_valid_sqlite_database(db_connection):
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
+    assert len(tables) > 0, "The database does not contain any tables, hence it is not a valid SQLite database."
 
 def test_tables_exist(db_cursor):
     db_cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
@@ -50,3 +68,23 @@ def test_table_data(db_cursor, table_name):
     db_cursor.execute(f"SELECT COUNT(*) FROM {table_name};")
     count = db_cursor.fetchone()[0]
     assert count > 0
+
+@pytest.mark.parametrize("table_name, column_name, min_value, max_value", [
+    ("temperature", "Y1961", -50, 50),
+    ("food_price_inflation", "Inflation", -100, 1000)
+])
+def test_data_validity(db_cursor, table_name, column_name, min_value, max_value):
+    query = f"SELECT {column_name} FROM {table_name} WHERE {column_name} NOT BETWEEN ? AND ?;"
+    db_cursor.execute(query, (min_value, max_value))
+    invalid_values = db_cursor.fetchall()
+    assert len(invalid_values) == 0, f"Found invalid values in column {column_name} of table {table_name}"
+
+@pytest.mark.parametrize("table_name, year_column_prefix, start_year, end_year", [
+    ("temperature", "Y", 1961, 2020),
+])
+def test_time_series_consistency(db_cursor, table_name, year_column_prefix, start_year, end_year):
+    for year in range(start_year, end_year + 1):
+        column_name = f"{year_column_prefix}{year}"
+        db_cursor.execute(f"SELECT COUNT(*) FROM {table_name} WHERE {column_name} IS NULL;")
+        null_count = db_cursor.fetchone()[0]
+        assert null_count == 0, f"Found NULL values in column {column_name} of table {table_name}"
--- project/test.sh
+++ project/test.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
-python test.py
 python pipeline.py
+python test.py
 
-data_file="../data/zylesto.sqlite"
-
-is_valid_sqlite_file() {
-
-    if [ ! -f "$data_file" ]; then
-        echo "Error: The specified path '$data_file' does not exist or is not a regular file path."
-        return 1
-    elif [ ! -r "$data_file" ]; then
-        echo "Error: The file '$data_file' is not readable."
-        return 1
-    fi
-
-    local file_header=$(head -c 16 "$data_file")
-    if [[ "$file_header" == "SQLite format 3"* ]]; then
-        echo "The file $data_file is a valid SQLite database."
-        return 0
-    else
-        echo "The file $data_file is not a valid SQLite database."
-        return 1
-    fi
-}
-
-# This method should work as a "System Test"
-is_valid_sqlite_file
 
 
